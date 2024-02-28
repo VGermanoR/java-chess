8e3b9ab5616c6a6be5f55791ead11b7fd85fc831
Vitor Germano <vitorgermanoramos@gmail.com>
2024-02-27 21:34:14 -0300

    add: boardgame Position class

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,10 @@
 package app;
 
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position pos = new Position(1, 2);
+        System.out.println(pos);
     }
 }
